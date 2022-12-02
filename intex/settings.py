c1c7efa432da5c0d466f898dd73c825be6300050
henry <henrydmatsen@gmail.com>
2022-12-01 20:10:30 -0700

    Add database

--- intex/settings.py
+++ intex/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,17 +78,11 @@ WSGI_APPLICATION = 'intex.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql', 
-        'NAME': 'intex',
-        'USER': 'postgres',
-        'PASSWORD': 'izzy123101', 
-        'HOST': 'localhost'
-    }
-}  
-
-
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
